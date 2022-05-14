9a527df794355ddd21edda2433d73775fc5edbe3
Jason Peng <mijasoda@gmail.com>
2022-05-14 21:52:41 +0800

    add test

--- test_budget_service.py
+++ test_budget_service.py
@@ -36,6 +36,43 @@ class TestBudgetService(unittest.TestCase):
 
         self.assertEqual(Decimal(Decimal(1000)), self.service.query(start, end))
 
+    def test_a_day_with_multi_month_budget(self):
+        self.given_repo_budgets([
+            Budget('202205', 31000),
+            Budget('202204', 60000),
+        ])
+
+        start = date(2022, 5, 1)
+        end = date(2022, 5, 1)
+
+        self.assertEqual(Decimal(Decimal(1000)), self.service.query(start, end))
+
+    def test_total_multi_month_budget(self):
+        self.given_repo_budgets([
+            Budget('202205', 31000),
+            Budget('202204', 60000),
+        ])
+
+        start = date(2022, 4, 1)
+        end = date(2022, 5, 31)
+
+        self.assertEqual(Decimal(31000) + Decimal(60000), self.service.query(
+            start,
+            end))
+
+    def test_partial_cross_month_multi_month_budget(self):
+        self.given_repo_budgets([
+            Budget('202205', 31000),
+            Budget('202204', 60000),
+        ])
+
+        start = date(2022, 4, 30)
+        end = date(2022, 5, 2)
+
+        self.assertEqual(2 * Decimal(1000) + 1 * Decimal(2000), self.service.query(
+            start,
+            end))
+
     def given_repo_budgets(self, budgets):
         self.fake_repo_all.return_value = budgets
 
