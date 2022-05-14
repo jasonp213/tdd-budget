2a3807e4c878e00da6aa420d00b76dbb8b3cb60e
Jason Peng <mijasoda@gmail.com>
2022-05-14 21:25:48 +0800

    add second test and make it green.

--- service.py
+++ service.py
@@ -3,5 +3,11 @@ from decimal import Decimal
 
 
 class BudgetService:
+    def __init__(self, repo):
+        self.budget_repo = repo
+
     def query(self, start: date, end: date) -> Decimal:
-        raise NotImplementedError
+        total = Decimal(0)
+        for budget in self.budget_repo.get_all():
+            total += Decimal(budget.amount)
+        return total
--- test_budget_service.py
+++ test_budget_service.py
@@ -1,19 +1,32 @@
 import unittest
 from datetime import date
 from decimal import Decimal
+from unittest.mock import patch
 
+from repo import Budget, BudgetRepo
 from service import BudgetService
 
 
 class TestBudgetService(unittest.TestCase):
     def setUp(self):
-        self.service = BudgetService()
+        patcher = patch('repo.BudgetRepo.get_all')
+        self.fake_repo_all = patcher.start()
+
+        self.service = BudgetService(BudgetRepo())
 
     def test_invalid_input_when_end_before_start(self):
         start = date(2022, 5, 10)
         end = date(2022, 4, 10)
 
         self.assertEqual(Decimal(0), self.service.query(start, end))
+
+    def test_a_whole_month(self):
+        self.fake_repo_all.return_value = [Budget('202205', 31000)]
+
+        start = date(2022, 5, 1)
+        end = date(2022, 5, 31)
+
+        self.assertEqual(Decimal(Decimal(31000)), self.service.query(start, end))
 
 
 if __name__ == '__main__':
